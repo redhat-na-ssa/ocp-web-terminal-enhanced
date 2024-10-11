281c1a767ceaee0d973457ea63a1b770ec038276
Cory Latschkowski <koree@redhat.com>
2024-10-11 11:04:53 -0500

    update: scripts / bins

--- container/download_tools.sh
+++ container/download_tools.sh
@@ -8,7 +8,7 @@ BASH_COMP=scratch/etc/bash_completion.d
 
 PATH=${BIN_PATH}:${PATH}
 
-[ -e bin.sh ] || curl -LO https://raw.githubusercontent.com/redhat-na-ssa/demo-ai-gitops-catalog/main/scripts/library/bin.sh
+[ -e bin.sh ] || curl -LO https://raw.githubusercontent.com/redhat-na-ssa/demo-ai-gitops-catalog/v0.06/scripts/library/bin.sh
 
 # shellcheck disable=SC1091
 . bin.sh
